c77c78fa0ae2b90e112d536e6804e9093a4f863d
Cole Voss <voss.cole@gmail.com>
2021-12-02 16:24:57 -0600

    remove google creds

--- infra/main.tf
+++ infra/main.tf
@@ -14,13 +14,13 @@ terraform {
 
 
 provider "google" {
-  credentials = var.gcp_credentials
-  project     = var.gcp_project_id
-  region      = var.gcp_region
+  # credentials = var.gcp_credentials
+  project = var.gcp_project_id
+  region  = var.gcp_region
 }
 
 provider "google-beta" {
-  credentials = var.gcp_credentials
-  project     = var.gcp_project_id
-  region      = var.gcp_region
+  # credentials = var.gcp_credentials
+  project = var.gcp_project_id
+  region  = var.gcp_region
 }
