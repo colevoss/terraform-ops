a0e6b715884cc980dfabe6d2f7cd112662b89ebc
Cole Voss <voss.cole@gmail.com>
2021-12-06 13:54:46 -0600

    clean up up to plan

---
+++ infra/deployment.tfvars
@@ -0,0 +1,3 @@
+gcp_project_id = "${GOOGLE_PROJECT_ID}"
+gcp_region = "${GCP_REGION}"
+my_service_image = "${MY_SERVICE_IMAGE}"
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,8 +1,3 @@
-# variable "gcp_credentials" {
-#   type      = string
-#   sensitive = true
-# }
-
 variable "gcp_project_id" {
   type = string
 }
